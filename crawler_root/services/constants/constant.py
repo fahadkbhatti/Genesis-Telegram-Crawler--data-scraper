3a61c94405eb3fafa7e24a1b33dae66bc23caf3b
fahad <fahad.bhatti@nuclieos.com>
2023-06-20 15:07:35 +0500

    first commit

---
+++ crawler_root/services/constants/constant.py
@@ -0,0 +1,14 @@
+import os
+
+
+class SHARED_CONSTANT:
+    S_PROJECT_PATH = ""
+
+class CLASSIFIER_PATH_CONSTANT:
+    S_CLASSIFIER_FOLDER_PATH = "raw/classifier_pickled_files"
+    S_DATASET_PATH = "raw/crawled_classifier_websites.csv"
+    S_CLASSIFIER_PICKLE_PATH = "raw/classifier_pickled_files/web_classifier.sav"
+    S_VECTORIZER_PATH = "raw/classifier_pickled_files/class_vectorizer.csv"
+    S_SELECTKBEST_PATH = "raw/classifier_pickled_files/feature_vector.sav"
+    S_TRAINING_DATA_PATH = "raw/classifier_pickled_files/training_data.csv"
+
---
+++ interface_manager/controller/constants/constant.py
@@ -0,0 +1,3 @@
+# mongoDB Database
+class constants:
+    S_DATABASE_DEFAULT_ENTRY = { 'm_username': 'admin', 'm_password': 'admin', 'm_role': 'admin' }
